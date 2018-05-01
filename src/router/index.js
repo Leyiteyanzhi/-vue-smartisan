4c524fb2be7782da8bb601107ca0f0d7270a078f
litianzhi <979638192@qq.com>
2018-05-01 23:50:59 +0800

    frist commit

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Shop from '@/views/shop'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/',
+    name: Shop,
+    component: Shop
+  }]
+})
