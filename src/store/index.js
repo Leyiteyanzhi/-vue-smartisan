821c6ef137acf24987c72abcd9c8a6292241a871
litianzhi <979638192@qq.com>
2018-05-02 17:02:55 +0800

    updated

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Vuex from 'Vuex'
+
+Vue.use(Vuex)
+
+let store = new Vuex.Store({
+  state: {
+    carPanelData: [
+
+    ]
+  },
+  mutations: {
+    addCarPanelData (state, data) {
+      let bOff = true
+      state.carPanelData.forEach((goods) => {
+        if (goods.sku_id === data.sku_id) {
+          goods.count++
+          bOff = false
+        }
+      })
+      if (bOff) {
+        let goodsData = data
+        Vue.set(goodsData, 'count', 1)
+        state.carPanelData.push(goodsData)
+      }
+      console.log(state.carPanelData)
+    }
+  }
+})
+
+export default store
